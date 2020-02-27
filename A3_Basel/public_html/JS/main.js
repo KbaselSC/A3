6797cb2e07db440f1111f4b199b0857a896272d3
KBaselSC <basel@sheridancollege.ca>
2020-02-27 17:42:14 -0500

    Changed it so my css changed cursor to the meso bags, and the kanna to the targets that chase the cursor. and made the kanna variables.

--- A3_Basel/public_html/JS/main.js
+++ A3_Basel/public_html/JS/main.js
@@ -4,4 +4,7 @@
  * and open the template in the editor.
  */
 
-
+//Declaring variables.
+var kanna1 = document.getElementById("Kanna1");
+var kanna2 = document.getElementById("Kanna2");
+var kanna3 = document.getElementById("Kanna3");
